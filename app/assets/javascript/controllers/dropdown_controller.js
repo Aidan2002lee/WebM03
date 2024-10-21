895f7d4aa1c715991e445f1a4ce0c3c140c6947a
Aidan Gardner <algardner2002@gmail.com>
2024-10-21 09:04:45 -0600

    init to m04 branch

---
+++ app/assets/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,7 @@
+import {Controller} from "@hotwired/stimulus"
+
+export default class extends Controller {
+    coneect() {
+        console.log("Bootstrap dropdown initialized");
+    }
+}
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,25 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+Student.find_each do |student|
+    student.profile_picture.purge if student.profile_picture.attached?
+end
+
+ActiveStorage::Blob.where.missing(:attachments).find_each(&:purge)
+
+Student.destroy_all
+
+50.times do |i|
+  student = Student.create!(
+    first_name: "First #{i + 1}",
+    last_name: "Last #{i + 1}",
+    school_email: "student#{i + 1}@msudenver.edu",
+    major: Student::VALID_MAJORS.sample,
+    graduation_date: Faker::Date.between(from: 2.years.ago, to: 2.years.from_now),
+  )
+    #profile_picture_url = "https://robohash.org/#{student.first_name.gsub('',")}"
+    #profile_picture = URI.open(profile_picture_url)
+    #student.profile_picture.attach(io: profile_picture, filename: "#{student.first_name}.jpg")
+end
+
 
